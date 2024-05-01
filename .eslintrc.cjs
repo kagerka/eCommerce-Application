fcddc46456facddc2aa68344e43d08aa642dbc66
Tatiana <138196468+Tanesha001@users.noreply.github.com>
2024-05-01 12:17:21 +0000

    feat: add airbnb

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,13 @@
 /* eslint-env node */
 module.exports = {
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
-    root: true,
-  };
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "airbnb-base",
+    "eslint-config-airbnb-base",
+    "airbnb-typescript/base",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  root: true,
+};
