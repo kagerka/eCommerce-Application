29578431a289295fb13036fc88a7e0fa929ef375
Tatiana <138196468+Tanesha001@users.noreply.github.com>
2024-05-01 19:10:50 +0000

    feat: add prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,13 @@
 /* eslint-env node */
 module.exports = {
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "airbnb-base",
-    "eslint-config-airbnb-base",
-    "airbnb-typescript/base",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb-base',
+    'eslint-config-airbnb-base',
+    'airbnb-typescript/base',
   ],
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   root: true,
 };
