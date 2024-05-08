e2385ab025b942a12f8d0029e5ee6da0bfc0a66c
Aleksandr Krivoshein <megalistnn@gmail.com>
2024-05-08 18:22:08 +0300

    feat: display clear error messages for validation issues

--- src/app/components/login-form/LoginForm.ts
+++ src/app/components/login-form/LoginForm.ts
@@ -7,6 +7,14 @@ import validateRegExp from '../../utils/validation/validateRegExp';
 import validateLeadingTrailingSpace from '../../utils/validation/validateLeadingTrailingSpace';
 
 const MIN_PASSWORD_LENGTH = 8;
+const RULE_FORMAT = 'Email address must be properly formatted (e.g., user@example.com).';
+const RULE_SPACE = 'Email address must not contain leading or trailing whitespace.';
+const RULE_DOMAIN_NAME = 'Email address must contain a domain name (e.g., example.com).';
+const RULE_SEPARATOR = 'Email address must contain an @ symbol separating local part and domain name.';
+const RULE_MIN_LENGTH = `🚫 Password must be at least ${MIN_PASSWORD_LENGTH} characters long.`;
+const RULE_CHAR =
+  '🚫 Password must contain at least one uppercase letter (A-Z), one lowercase letter (a-z), one digit (0-9), one special character(e.g., !@#$%^&*)';
+const RULE_PSW_SPACE = '🚫 Password must not contain leading or trailing whitespace.';
 
 class LoginForm {
   private passwordInputStatus: boolean;
@@ -41,6 +49,28 @@ class LoginForm {
 
   private loginButton: Button;
 
+  private tooltipContainer: BaseComponent;
+
+  private tooltipIcon: BaseComponent;
+
+  private tooltipMessage: BaseComponent;
+
+  private tooltipList: BaseComponent;
+
+  private tooltipRuleFormat: BaseComponent;
+
+  private tooltipRuleSpace: BaseComponent;
+
+  private tooltipRuleDomain: BaseComponent;
+
+  private tooltipRuleSeparator: BaseComponent;
+
+  private pswRuleLength: BaseComponent;
+
+  private pswRuleChar: BaseComponent;
+
+  private pswRuleSpace: BaseComponent;
+
   constructor() {
     this.passwordInputStatus = false;
     this.emailInputStatus = false;
@@ -58,15 +88,28 @@ class LoginForm {
     this.showPswBtn = LoginForm.createShowPasswordBtnElement();
     this.passwordError = LoginForm.createPasswordErrorElement();
     this.loginButton = LoginForm.createLoginButtonElement();
-
+    this.tooltipContainer = LoginForm.createTooltipContainerElement();
+    this.tooltipIcon = LoginForm.createTooltipIconElement();
+    this.tooltipMessage = LoginForm.createTooltipMessageElement();
+    this.tooltipList = LoginForm.createTooltipListElement();
+    this.tooltipRuleFormat = LoginForm.createTooltipItemElement(RULE_FORMAT);
+    this.tooltipRuleSpace = LoginForm.createTooltipItemElement(RULE_SPACE);
+    this.tooltipRuleDomain = LoginForm.createTooltipItemElement(RULE_DOMAIN_NAME);
+    this.tooltipRuleSeparator = LoginForm.createTooltipItemElement(RULE_SEPARATOR);
+    this.pswRuleLength = LoginForm.createPswRuleElement(RULE_MIN_LENGTH);
+    this.pswRuleChar = LoginForm.createPswRuleElement(RULE_CHAR);
+    this.pswRuleSpace = LoginForm.createPswRuleElement(RULE_PSW_SPACE);
     this.composeView();
-
     this.handlePasswordInput();
     this.handleEmailInput();
     this.handleShowPswBtn();
   }
 
   private composeView(): void {
+    this.tooltipList.html.append(this.tooltipRuleFormat.html, this.tooltipRuleSpace.html);
+    this.tooltipList.html.append(this.tooltipRuleDomain.html, this.tooltipRuleSeparator.html);
+    this.tooltipMessage.html.append(this.tooltipList.html);
+    this.tooltipContainer.html.append(this.tooltipIcon.html, this.tooltipMessage.html);
     this.emailInputContainer.html.append(this.emailInput.view.html, this.emailError.html);
     this.emailField.html.append(this.emailLabel.html, this.emailInputContainer.html);
     this.passwordInputContainer.html.append(this.passwordInput.view.html);
@@ -85,7 +128,7 @@ class LoginForm {
   }
 
   private static createEmailFieldElement(): BaseComponent {
-    return new BaseComponent({ tag: 'div', class: ['login-field', 'login-field-name'] });
+    return new BaseComponent({ tag: 'div', class: ['login-field', 'login-field-email'] });
   }
 
   private static createEmailLabelElement(): BaseComponent {
@@ -106,7 +149,7 @@ class LoginForm {
 
   private static createEmailInputElement(): Input {
     return new Input({
-      type: 'email',
+      type: 'text',
       class: ['login-input', 'login-input-email'],
       placeholder: 'Enter your email',
     });
@@ -115,7 +158,7 @@ class LoginForm {
   private static createEmailErrorElement(): BaseComponent {
     return new BaseComponent({
       tag: 'div',
-      class: ['login-error', 'login-error-name'],
+      class: ['login-error', 'login-error-email'],
     });
   }
 
@@ -157,7 +200,7 @@ class LoginForm {
   private static createPasswordErrorElement(): BaseComponent {
     return new BaseComponent({
       tag: 'div',
-      class: ['login-error', 'login-error-name'],
+      class: ['login-error', 'login-error-password'],
     });
   }
 
@@ -168,6 +211,56 @@ class LoginForm {
       text: 'Login',
       disabled: true,
     });
+  }
+
+  private static createTooltipContainerElement(): BaseComponent {
+    return new BaseComponent({
+      tag: 'div',
+      class: ['tooltip-container'],
+    });
+  }
+
+  private static createTooltipIconElement(): BaseComponent {
+    return new BaseComponent({
+      tag: 'div',
+      class: ['tooltip-icon'],
+    });
+  }
+
+  private static createTooltipMessageElement(): BaseComponent {
+    return new BaseComponent({
+      tag: 'div',
+      class: ['tooltip-message'],
+    });
+  }
+
+  private static createTooltipListElement(): BaseComponent {
+    return new BaseComponent({
+      tag: 'ul',
+      class: ['tooltip-list'],
+    });
+  }
+
+  private static createTooltipItemElement(text: string): BaseComponent {
+    return new BaseComponent({
+      tag: 'li',
+      class: ['tooltip-item'],
+      text,
+    });
+  }
+
+  private static createPswRuleElement(text: string): BaseComponent {
+    return new BaseComponent({
+      tag: 'div',
+      class: ['error-message-item'],
+      text,
+    });
+  }
+
+  private static cleanInsideElement(element: HTMLElement): void {
+    while (element.firstChild) {
+      element.removeChild(element.firstChild);
+    }
   }
 
   private handleShowPswBtn(): void {
@@ -193,18 +286,39 @@ class LoginForm {
     }
   }
 
+  private static addClassError(elemen: HTMLElement): void {
+    elemen.classList.remove('success');
+    elemen.classList.add('error');
+  }
+
+  private static addClassSuccess(elemen: HTMLElement): void {
+    elemen.classList.remove('error');
+    elemen.classList.add('success');
+  }
+
   private validateEmailInput(): void {
     if (this.emailInput.view.html instanceof HTMLInputElement) {
+      const errorFormat = new BaseComponent({
+        tag: 'div',
+        class: ['error-message'],
+        text: '🚫 Incorrect email address format',
+      });
+      errorFormat.html.append(this.tooltipContainer.html);
+
       const { value } = this.emailInput.view.html;
       const regExp =
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
       const isValidateRegExp = validateRegExp(value, regExp);
       const isValidateLeadingTrailingSpace = validateLeadingTrailingSpace(value);
-
       if (isValidateRegExp && isValidateLeadingTrailingSpace) {
         this.emailInputStatus = true;
+        LoginForm.addClassSuccess(this.emailInput.view.html);
+        LoginForm.cleanInsideElement(this.emailError.html);
       } else {
         this.emailInputStatus = false;
+        LoginForm.addClassError(this.emailInput.view.html);
+        LoginForm.cleanInsideElement(this.emailError.html);
+        this.emailError.html.append(errorFormat.html);
       }
     }
   }
@@ -218,6 +332,7 @@ class LoginForm {
 
   private validatePasswordInput(): void {
     if (this.passwordInput.view.html instanceof HTMLInputElement) {
+      LoginForm.cleanInsideElement(this.passwordError.html);
       const { value } = this.passwordInput.view.html;
       const passwordLength = value.length;
       const regExp = /(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])/g;
@@ -227,8 +342,22 @@ class LoginForm {
 
       if (isValidateLength && isValidateRegExp && isValidateLeadingTrailingSpace) {
         this.passwordInputStatus = true;
-      } else {
+        LoginForm.addClassSuccess(this.passwordInput.view.html);
+      }
+      if (!isValidateLength) {
         this.passwordInputStatus = false;
+        LoginForm.addClassError(this.passwordInput.view.html);
+        this.passwordError.html.append(this.pswRuleLength.html);
+      }
+      if (!isValidateRegExp) {
+        this.passwordInputStatus = false;
+        LoginForm.addClassError(this.passwordInput.view.html);
+        this.passwordError.html.append(this.pswRuleChar.html);
+      }
+      if (!isValidateLeadingTrailingSpace) {
+        this.passwordInputStatus = false;
+        LoginForm.addClassError(this.passwordInput.view.html);
+        this.passwordError.html.append(this.pswRuleSpace.html);
       }
     }
   }
