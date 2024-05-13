1f8bb65d3df7389d680b41817cd01ef9326f2c6f
kagerka <kagerka@gmail.com>
2024-05-13 09:52:13 +0300

    feat: show main page on the app load

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,22 +1,22 @@
 import './app.scss';
 import BaseComponent from './components/BaseComponent';
-import Login from './pages/login/Login';
+import MainPage from './pages/main/MainPage';
 
 class App {
   private static container: HTMLElement = document.body;
 
   private content: BaseComponent;
 
-  private loginPage: Login;
+  private mainPage: MainPage;
 
   constructor() {
     this.content = new BaseComponent({ tag: 'div', class: ['app'] });
-    this.loginPage = new Login();
+    this.mainPage = new MainPage();
   }
 
   run(): void {
     App.container.append(this.content.html);
-    this.content.html.append(this.loginPage.view.html);
+    this.content.html.append(this.mainPage.view.html);
   }
 }
 
