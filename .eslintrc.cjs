ed877195921021d2aa0cbcf21772f20a12f411f5
Ekaterina Kotliarenko <kagerka@gmail.com>
2024-05-13 20:14:27 +0300

    Feat: RSS-ECOMM-2_17: Add links to all functional pages on the main page (#116)
    * feat: add Header component
    
    * feat: add banner to Main page
    
    * feat: add Footer component
    
    * feat: add Main Page
    
    * fix: fix eslint and prettier rules
    
    * feat: show main page on the app load
    
    * fix: fix merge conflict

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -25,6 +25,7 @@ module.exports = {
     'no-magic-numbers': ['error', { ignoreArrayIndexes: true }],
     '@typescript-eslint/explicit-function-return-type': 'error',
     'max-lines-per-function': ['error', { max: 40, skipBlankLines: true }],
+    'max-len': ['error', { code: 120, ignoreStrings: true, ignoreRegExpLiterals: true }],
   },
   overrides: [
     {
