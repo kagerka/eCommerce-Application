10f02084b3c2194000d8e224405d5d4ea4c831bf
Tatiana <138196468+Tanesha001@users.noreply.github.com>
2024-06-01 14:16:17 +0000

    feat: enable card clicking

--- src/app/utils/validation/validateEmailAdress.ts
+++
@@ -1,9 +0,0 @@
-function validateEmailAdress(value: string): boolean {
-  const validEmailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-  if (!value.match(validEmailRegex)) {
-    return false;
-  }
-  return true;
-}
-
-export default validateEmailAdress;
