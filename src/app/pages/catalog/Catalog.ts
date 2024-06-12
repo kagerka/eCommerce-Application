f86eb82eca53f5a9b26f88a3399b5c9abdf2f919
Tatiana <138196468+Tanesha001@users.noreply.github.com>
2024-06-12 14:24:08 +0000

    feat: add cart icon and empty state

--- src/app/pages/catalog/Catalog.ts
+++ src/app/pages/catalog/Catalog.ts
@@ -4,7 +4,17 @@ import BaseComponent from '../../components/BaseComponent';
 import Banner from '../../components/banner/Banner';
 import Products from '../../components/products/Products';
 
+import { IQueryProducts } from '../../interfaces/Product.interface';
+
 import './Catalog.scss';
+
+const products: IQueryProducts = {
+  limit: 70,
+  offset: 0,
+  count: 0,
+  total: 0,
+  results: [],
+};
 
 class Catalog {
   private main: BaseComponent;
@@ -18,14 +28,9 @@ class Catalog {
     this.banner = new Banner();
     this.products = new Products();
     this.composeView();
-    Catalog.displayBrands();
   }
 
   private composeView(): void {
-    localStorage.removeItem('loadedProducts');
-    localStorage.removeItem('currentCategoryID');
-    localStorage.removeItem('currentBrand');
-    Products.loadMoreButton.view.html.removeAttribute('disabled');
     this.main.html.append(this.banner.view.html, this.products.view.html);
   }
 
@@ -33,54 +38,23 @@ class Catalog {
     return new BaseComponent({ tag: 'main', class: ['main-content'] });
   }
 
-  static async displayCatalog(): Promise<void> {
-    Products.productsList.html.innerHTML = '';
-    Catalog.handleCatalog();
-  }
-
-  static async handleCatalog(): Promise<void> {
-    const token = localStorage.getItem('tokenPassword')
-      ? localStorage.getItem('tokenPassword')
-      : localStorage.getItem('tokenAnonymous');
-    const pageNumber = 1;
-    const ONE = 1;
-    if (token) {
-      try {
-        await ECommerceApi.getProducts(currentClient, token, pageNumber)
-          .then(async (resp) => Products.getProductCards(resp, true))
-          .then((resp) => {
-            if (resp.resp.offset + resp.resp.count < resp.resp.total && !localStorage.getItem('currentCategoryID')) {
-              ECommerceApi.getProducts(currentClient, token, pageNumber + ONE).then(() => {
-                Products.disableLoadButton(false);
-              });
-            } else {
-              Products.disableLoadButton(true);
-            }
-            return resp.productCards;
-          })
-          .then(async (productCards) => Products.displayProductCards(productCards));
-      } catch (error) {
-        console.error(`Error displayProducts: ${error}`);
-      }
-      Products.handleLoadProductsAllButton();
-    }
-  }
-
-  static async displayBrands(): Promise<void> {
+  static async displayProducts(): Promise<void> {
     const token = localStorage.getItem('tokenPassword')
       ? localStorage.getItem('tokenPassword')
       : localStorage.getItem('tokenAnonymous');
     if (token) {
       try {
-        const allProducts = await ECommerceApi.getAllProducts(currentClient, token);
-        localStorage.setItem('allProducts', JSON.stringify(allProducts.results));
-        await Products.createProductCardsFromLocalStorage(true).forEach(() => {
+        const res = await ECommerceApi.getProducts(currentClient, token);
+        products.results = res.results;
+        localStorage.setItem('products', JSON.stringify(products.results));
+        await Products.createProductCardsFromLocalStorage(true).forEach((productCard) => {
+          Products.productsList.html.append(productCard.html);
           const brands = Products.displayBrands();
           Products.brandsContainer.html.innerHTML = '';
           Products.brandsContainer.append(brands.html);
         });
       } catch (error) {
-        console.error(`Error displayBrands: ${error}`);
+        throw new Error(`Error displayProducts: ${error}`);
       }
     }
   }
@@ -106,7 +80,7 @@ class Catalog {
           });
         });
       } catch (error) {
-        console.error(`Error displayCategories: ${error}`);
+        throw new Error(`Error displayCategories: ${error}`);
       }
     }
   }
